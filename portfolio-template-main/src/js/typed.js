abf5ce3b02d2b2abf62d3477ba655443c8b9aa00
himabindutellakula <tellakulahimabindu26@gmail.com>
2024-01-01 22:51:31 -0500

    First commit

---
+++ portfolio-template-main/src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js'
+
+var options = {
+  strings: ["I'm a 5X Certified Salesforce Developer", "I'm a Full-Stack Developer", "I'm a Business Technology Analyst", "I Solve Problems"],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true,
+}
+
+var typed = new Typed('#headline', options)

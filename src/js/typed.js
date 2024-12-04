b5bda8dc0fe2b88878ee3348ab6da4e8581bd5e9
himabindutellakula <tellakulahimabindu26@gmail.com>
2024-12-03 21:40:30 -0500

    Made basic changes to portfolio

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js'
 
 var options = {
-  strings: ["I'm a 5X Certified Salesforce Developer", "I'm a Full-Stack Developer", "I'm a Business Technology Analyst", "I Solve Problems"],
+  strings: ["I'm a Software Engineer", "I'm a Full-Stack Developer", "I'm a 6X Certified Salesforce Developer"],
   typeSpeed: 120,
   backSpeed: 50,
   backDelay: 1500,
